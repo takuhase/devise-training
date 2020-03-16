e8104b7356e230ebb4d2f976f6fc2376c5055aee
EC2 Default User <ec2-user@ip-172-31-47-183.ap-northeast-1.compute.internal>
2020-03-16 08:05:04 +0000

    Install Bootstrap and activate i18n

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module DeviseSample
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # 以下を追加すれば日本語に
+    config.i18n.default_locale = :ja
+    # タイムゾーンも変更するなら，以下を追加
+    config.time_zone = 'Asia/Tokyo'
   end
 end
