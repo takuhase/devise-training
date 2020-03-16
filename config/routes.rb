551b0cdd2f679f704d5fd32f5b1c5bca1042f892
EC2 Default User <ec2-user@ip-172-31-47-183.ap-northeast-1.compute.internal>
2020-03-16 14:29:18 +0000

    Complete guest user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'homes/index'
   root 'homes#index'
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
